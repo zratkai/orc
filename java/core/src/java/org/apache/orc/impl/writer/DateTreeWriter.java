57a33aada4b490d76875d3c46b3b95d52788baf6
Owen O'Malley <omalley@apache.org>
2019-06-17 19:31:58 +0200

    CDPD-45931: ORC-486: Add column entryption to the TreeWriters.
    Fixes #403
    
    Signed-off-by: Owen O'Malley <omalley@apache.org>
    Change-Id: I1df1c7d5b104ac4ab182717a6a43709ac5394d97

--- java/core/src/java/org/apache/orc/impl/writer/DateTreeWriter.java
+++ java/core/src/java/org/apache/orc/impl/writer/DateTreeWriter.java
@@ -24,9 +24,11 @@ import org.apache.hadoop.hive.ql.exec.vector.LongColumnVector;
 import org.apache.hadoop.hive.ql.util.JavaDataModel;
 import org.apache.orc.OrcProto;
 import org.apache.orc.TypeDescription;
+import org.apache.orc.impl.CryptoUtils;
 import org.apache.orc.impl.IntegerWriter;
 import org.apache.orc.impl.OutStream;
 import org.apache.orc.impl.PositionRecorder;
+import org.apache.orc.impl.StreamName;
 
 import java.io.IOException;
 
@@ -35,13 +37,12 @@ public class DateTreeWriter extends TreeWriterBase {
   private final boolean isDirectV2;
   private final boolean useProleptic;
 
-  public DateTreeWriter(int columnId,
-                        TypeDescription schema,
-                        WriterContext writer,
-                        boolean nullable) throws IOException {
-    super(columnId, schema, writer, nullable);
-    OutStream out = writer.createStream(id,
-        OrcProto.Stream.Kind.DATA);
+  public DateTreeWriter(TypeDescription schema,
+                        WriterEncryptionVariant encryption,
+                        WriterContext writer) throws IOException {
+    super(schema, encryption, writer);
+    OutStream out = writer.createStream(
+        new StreamName(id, OrcProto.Stream.Kind.DATA, encryption));
     this.isDirectV2 = isNewWriteFormat(writer);
     this.writer = createIntegerWriter(out, true, isDirectV2, writer);
     if (rowIndexPosition != null) {
@@ -93,10 +94,8 @@ public class DateTreeWriter extends TreeWriterBase {
   }
 
   @Override
-  public void writeStripe(OrcProto.StripeFooter.Builder builder,
-                          OrcProto.StripeStatistics.Builder stats,
-                          int requiredIndexEntries) throws IOException {
-    super.writeStripe(builder, stats, requiredIndexEntries);
+  public void writeStripe(int requiredIndexEntries) throws IOException {
+    super.writeStripe(requiredIndexEntries);
     if (rowIndexPosition != null) {
       recordPosition(rowIndexPosition);
     }
@@ -136,4 +135,9 @@ public class DateTreeWriter extends TreeWriterBase {
     writer.flush();
   }
 
+  @Override
+  public void prepareStripe(int stripeId) {
+    super.prepareStripe(stripeId);
+    writer.changeIv(CryptoUtils.modifyIvForStripe(stripeId));
+  }
 }
