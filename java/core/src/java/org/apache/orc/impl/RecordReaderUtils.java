ae42a45e593dbb587f58758cae50718b86efddfc
Owen O'Malley <omalley@apache.org>
2019-07-15 11:33:01 -0700

    CDPD-45931: ORC-520: Fix file merging for column encryption.
    Fixes #423
    
    Signed-off-by: Owen O'Malley <omalley@apache.org>
    Change-Id: Ic627fa372c3d2a4e6865615986c58358ed0b9a10

--- java/core/src/java/org/apache/orc/impl/RecordReaderUtils.java
+++ java/core/src/java/org/apache/orc/impl/RecordReaderUtils.java
@@ -246,8 +246,8 @@ public class RecordReaderUtils {
     }
 
     @Override
-    public CompressionCodec getCompressionCodec() {
-      return options.getCodec();
+    public InStream.StreamOptions getCompressionOptions() {
+      return options;
     }
   }
 
@@ -270,7 +270,8 @@ public class RecordReaderUtils {
     return rightB >= leftA;
   }
 
-  public static long estimateRgEndOffset(InStream.StreamOptions unencryptedOptions,
+  public static long estimateRgEndOffset(boolean isCompressed,
+                                         int bufferSize,
                                          boolean isLast,
                                          long nextGroupOffset,
                                          long streamLength) {
@@ -282,9 +283,9 @@ public class RecordReaderUtils {
     long slop = WORST_UNCOMPRESSED_SLOP;
     // Stretch the slop by a factor to safely accommodate following compression blocks.
     // We need to calculate the maximum number of blocks(stretchFactor) by bufferSize accordingly.
-    if (unencryptedOptions.isCompressed()) {
-      int stretchFactor = 2 + (MAX_VALUES_LENGTH * MAX_BYTE_WIDTH - 1) / unencryptedOptions.getBufferSize();
-      slop = stretchFactor * (OutStream.HEADER_SIZE + unencryptedOptions.getBufferSize());
+    if (isCompressed) {
+      int stretchFactor = 2 + (MAX_VALUES_LENGTH * MAX_BYTE_WIDTH - 1) / bufferSize;
+      slop = stretchFactor * (OutStream.HEADER_SIZE + bufferSize);
     }
     return isLast ? streamLength : Math.min(streamLength, nextGroupOffset + slop);
   }
@@ -348,6 +349,7 @@ public class RecordReaderUtils {
         }
         return base + BYTE_STREAM_POSITIONS + compressionValue;
       case TIMESTAMP:
+      case TIMESTAMP_INSTANT:
         if (streamType == OrcProto.Stream.Kind.DATA) {
           return base;
         }
